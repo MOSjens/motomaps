f8f88143d756d3dec8942a6d483dffbd6826f7f0
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-14 09:10:25 +0000

    Deploy MOSjens/motomaps to github.com/MOSjens/motomaps.git:gh-pages

--- main.js
+++ main.js
@@ -1,17 +1,82 @@
 import {Map, View} from 'ol';
-import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
-import XYZ from 'ol/source/XYZ';
+import GPX from 'ol/format/GPX';
+import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer';
+import VectorSource from 'ol/source/Vector';
+import {Circle as CircleStyle, Fill, Stroke, Style} from 'ol/style';
 
-const raster = new TileLayer({
-	source: new OSM()
-});
+	const raster = new Tile({
+        source: new OSM()
+        });
 
-new Map({
-  target: 'map',
-  layers: [raster],
-  view: new View({
-    center: [962819.917537, 6310344.861740], //Coordinates of Bad Schönborn
-    zoom: 12
-  })
-});
+      const style = {
+        'Point': new Style({
+          image: new CircleStyle({
+            fill: new Fill({
+              color: 'rgba(255,255,0,0.4)'
+            }),
+            radius: 5,
+            stroke: new Stroke({
+              color: '#ff0',
+              width: 1
+            })
+          })
+        }),
+        'LineString': new Style({
+          stroke: new Stroke({
+            color: '#f00',
+            width: 3
+          })
+        }),
+        'MultiLineString': new Style({
+          stroke: new Stroke({
+            color: '#0f0',
+            width: 3
+          })
+        })
+      };
+      const vector = new VectorLayer({
+        source: new VectorSource({
+         url: 'gpx/2019-05-18_09-53_Sat.gpx',
+          format: new GPX()
+        }),
+        style: function(feature) {
+          return style[feature.getGeometry().getType()];
+        }
+      });
+      const map = new Map({
+        layers: [raster],     //, vector],
+        target: document.getElementById('map'),
+        view: new View({
+          center: [962819.917537, 6310344.861740], //Coordinates of Bad Schönborn
+          zoom: 12
+        })
+      });
+      const displayFeatureInfo = function(pixel) {
+        const features = [];
+        map.forEachFeatureAtPixel(pixel, function(feature) {
+          features.push(feature);
+        });
+        if (features.length > 0) {
+          const info = [];
+          let i, ii;
+          for (i = 0, ii = features.length; i < ii; ++i) {
+            info.push(features[i].get('desc'));
+          }
+          document.getElementById('info').innerHTML = info.join(', ') || '(unknown)';
+          map.getTarget().style.cursor = 'pointer';
+        } else {
+          document.getElementById('info').innerHTML = '&nbsp;';
+          map.getTarget().style.cursor = '';
+        }
+      };
+      map.on('pointermove', function(evt) {
+        if (evt.dragging) {
+          return;
+        }
+        const pixel = map.getEventPixel(evt.originalEvent);
+        displayFeatureInfo(pixel);
+      });
+      map.on('click', function(evt) {
+        displayFeatureInfo(evt.pixel);
+      });
