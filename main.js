537509769d18f68215af34e4476728a732fcb8e9
reinhart <jens.reinhart@disy.net>
2019-08-14 09:53:25 +0200

    chenged to osm

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
 import {Map, View} from 'ol';
 import TileLayer from 'ol/layer/Tile';
+import OSM from '/ol/source/OSM';
 import XYZ from 'ol/source/XYZ';
+
+const raster = new Tile({
+	source: new OSM()
+});
 
 new Map({
   target: 'map',
-  layers: [
-    new TileLayer({
-      source: new XYZ({
-        url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-      })
-    })
-  ],
+  layers: [raster],
   view: new View({
-    center: [0, 0],
-    zoom: 2
+    center: [962819.917537, 6310344.861740], //Coordinates of Bad Schönborn
+    zoom: 12
   })
 });
